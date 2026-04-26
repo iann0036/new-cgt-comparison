20842c99c22cde3005403350b40520c013b4efc2
Ian Mckay <contact@ianmckay.com.au>
2026-04-26 15:34:53 +1000

    New base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/new-cgt-comparison/',
+  base: '/new-cgt-comparison/dist/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
